6336a5ffbc56745bc4807bef8e438e27fed06a24
Daniel-Cas <daniel.achury@rappi.com>
2024-05-05 15:36:16 -0500

    Initial commit
    Add health check controller

---
+++ src/main/java/com/greenyapi/GreenyApiApplication.java
@@ -0,0 +1,13 @@
+package com.greenyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenyApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/greenyapi/GreenyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
